670ac10c2d8a312e8afb1b5ed64423269d3ab758
AfokePeniel <afokekowho@gmail.com>
2025-08-25 23:04:48 -0400

    solved C, C2,C3

---
+++ challanges1-11/C1.py
@@ -0,0 +1,9 @@
+'''
+Ask for the user’s first name and
+display the output message: 
+Hello [First Name] .
+'''
+#Solution
+
+first_name = input("Kindly enter your first name \n")
+print("Hello" + " " + first_name)
